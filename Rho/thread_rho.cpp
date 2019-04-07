b74858585bf59cc031140a950b338398157a25a8
thomasf2 <thomasf2@umbc.edu>
2019-04-07 18:03:34 -0400

    working (probably) multithreaded Rho

--- Rho/thread_rho.cpp
+++ Rho/thread_rho.cpp
@@ -4,8 +4,10 @@
 #include "time.h"
 #include "gmp.h"
 #include "gmpxx.h"
-#include "assert.h"
-@include "pthread.h"
+//#include "pthread.h"
+#include <thread>
+#include <atomic>
+#include <ctime>
 
 bool isprime(mpz_class n){
   if (n == 2){return true; }
@@ -53,35 +55,41 @@ mpz_class gcd(mpz_class a,mpz_class b){
   return a;
 }
 */
-mpz_class pollard_rho(mpz_class n)  //pollard rho implementation
-{
-    if(n%2==0)
-        return 2;
+std::atomic<bool> exit_thread_flag{false};
+clock_t cl;
 
-    mpz_class x = rand()%n+1;
-    mpz_class c = rand()%n+1;
-    mpz_class y = x;
+void*  pollard_rho(mpz_class x, mpz_class y, mpz_class c, mpz_class n )  //pollard rho implementation
+{
+  if(n%2==0)
+    printf("EVEN\n");
+  else{
+    //mpz_class x = rand()%n+1;
+    //mpz_class c = rand()%n+1;
+    //mpz_class y = x;
     mpz_class g = 1;
     //printf("POLLARD RHO\n");
     //printf("INITIAL: x: %d | y: %d | c: %d | g: %d\n", x,y,c,g);
     int iteration = 0;
-    while(g==1){
+    while(g==1 and not exit_thread_flag){
       x = ( (x*x)%n + c)%n;    
       y = ( (y*y)%n + c)%n;
       y = ( (y*y)%n + c)%n;
-      //printf("x: %d | y: %d | c: %d | g %d\n", x,y,c,g);
-      
-      g = gcd(abso(x-y),n);
-      //gmp_printf("Recurrence depth %d, g %Zd iteration %d\n", recur_depth, g, iteration);
-      //recur_depth = 0;
-      //iteration ++;
-      //printf("GCD of %d and %d = %d\n", abso(x-y), n, g);
-      //printf("\n");
+      g = gcd(abso(x-y),  n);
+
     }
-    return g;
+
+    if(exit_thread_flag == false){
+      double duration = ( clock() - cl )/(double)CLOCKS_PER_SEC;
+      gmp_printf("Hey, I Found it! The factor is %Zd and my id is %d\n", g, std::this_thread::get_id());
+      printf("That took %f seconds\n", duration);
+    }
+
+    exit_thread_flag = true;
+    
+    
+  }
+    //return g;
 }
-
-
 
 
 int main(int argc, char *argv[])  //Driver Program
@@ -91,13 +99,31 @@ int main(int argc, char *argv[])  //Driver Program
     mpz_class n(argv[1]);
     mpz_class p;
     mpz_class q;
-    int thread_id[8];
-    pthread_t threads[8];
+    mpz_class returnVal[8];
+
+    mpz_class x;// = rand()%n+1;
+    mpz_class c;// = rand()%n+1;
+    mpz_class y;// = x;
     
-    for(int i = 0 i < 8; i++){
-      thread_id[i] = pthread_create(&threads[i], NULL, pollard_rho, n); 
+    std::thread threads[8];
+    gmp_printf("Starting threads for number %Zd\n", n);
+    cl = clock();
+    for(int i = 0; i < 8; i++){
+       x = rand()%n+1;
+       c = rand()%n+1;
+       y = x;
+
+       threads[i] = std::thread(pollard_rho, x, y, c, n);
+      
+    }
+
+    for(int i = 0; i < 8; i++){
+      //      printf("Joining threads[%d]\n", i);
+      threads[i].join();
+      
     }
     
+    /*
     gmp_printf("Read Input number: %Zd\n", n);
     gmp_printf("srarting pollard rho\n");
     p = pollard_rho(n);
@@ -110,7 +136,7 @@ int main(int argc, char *argv[])  //Driver Program
     gmp_printf("n: %Zd\n", n);
     gmp_printf("p: %Zd\n", p);
     gmp_printf("q: %Zd\n", q);
-      
+    */
       
 	return 0;
 }
