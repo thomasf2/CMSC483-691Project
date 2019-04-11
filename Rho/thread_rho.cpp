6381c7bbde1572bccf392bf7a07f8539b9d4d44b
thomasf2 <thomasf2@umbc.edu>
2019-04-11 17:55:47 -0400

    added threaded rho executable and source

--- Rho/thread_rho.cpp
+++ Rho/thread_rho.cpp
@@ -1,14 +1,14 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "string.h"
-#include "time.h"
-#include "gmp.h"
-#include "gmpxx.h"
+#include <gmpxx.h>
 //#include "pthread.h"
 #include <thread>
 #include <atomic>
 #include <ctime>
-
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+/*
 bool isprime(mpz_class n){
   if (n == 2){return true; }
   if (n == 3){return true; }
@@ -27,7 +27,7 @@ bool isprime(mpz_class n){
   return true;
 
 }
-
+*/
 
 mpz_class abso(mpz_class a)     //fn to return absolute value
 {
@@ -44,99 +44,75 @@ mpz_class gcd(mpz_class a,mpz_class b)    //Euclidean GCD recursive fn
     }
 }
 
-/*
-mpz_class gcd(mpz_class a,mpz_class b){
-  mpz_class t;
-  while(b != 0){
-    t = b;
-    b = a % b;
-    a = t;
-  }
-  return a;
-}
-*/
-std::atomic<bool> exit_thread_flag{false};
-clock_t cl;
-
-void*  pollard_rho(mpz_class x, mpz_class y, mpz_class c, mpz_class n )  //pollard rho implementation
-{
-  if(n%2==0)
-    printf("EVEN\n");
-  else{
-    //mpz_class x = rand()%n+1;
-    //mpz_class c = rand()%n+1;
-    //mpz_class y = x;
-    mpz_class g = 1;
-    //printf("POLLARD RHO\n");
-    //printf("INITIAL: x: %d | y: %d | c: %d | g: %d\n", x,y,c,g);
-    int iteration = 0;
-    while(g==1 and not exit_thread_flag){
-      x = ( (x*x)%n + c)%n;    
-      y = ( (y*y)%n + c)%n;
-      y = ( (y*y)%n + c)%n;
-      g = gcd(abso(x-y),  n);
-
-    }
-
-    if(exit_thread_flag == false){
-      double duration = ( clock() - cl )/(double)CLOCKS_PER_SEC;
-      gmp_printf("Hey, I Found it! The factor is %Zd and my id is %d\n", g, std::this_thread::get_id());
-      printf("That took %f seconds\n", duration);
-    }
-
-    exit_thread_flag = true;
-    
-    
-  }
-    //return g;
-}
 
 
 int main(int argc, char *argv[])  //Driver Program
 {
     srand(time(NULL));
-
+    if(argc != 3){
+      std::cout << "Two arguments plz" << std::endl;
+      std::cout << "1: the number to factor, 2: the number of threads" << std::endl;
+      return -1;
+    }
     mpz_class n(argv[1]);
-    mpz_class p;
+    int num_threads = atoi(argv[2]);
+    mpz_class p = 0;
     mpz_class q;
-    mpz_class returnVal[8];
 
-    mpz_class x;// = rand()%n+1;
-    mpz_class c;// = rand()%n+1;
-    mpz_class y;// = x;
-    
-    std::thread threads[8];
-    gmp_printf("Starting threads for number %Zd\n", n);
-    cl = clock();
-    for(int i = 0; i < 8; i++){
-       x = rand()%n+1;
-       c = rand()%n+1;
-       y = x;
+    std::mutex m;
+    std::condition_variable cond;
+    std::atomic<bool> running(true);
 
-       threads[i] = std::thread(pollard_rho, x, y, c, n);
-      
-    }
-
-    for(int i = 0; i < 8; i++){
-      //      printf("Joining threads[%d]\n", i);
-      threads[i].join();
-      
+    if(n%2==0){
+      printf("EVEN\n");
+      return -1;
     }
     
-    /*
-    gmp_printf("Read Input number: %Zd\n", n);
-    gmp_printf("srarting pollard rho\n");
-    p = pollard_rho(n);
-    gmp_printf("done pollard rho, finding q by division\n");
-    q = n / p;
+    std::thread threads[num_threads];
+    std::cout << "Starting threads for number " << n << std::endl;
+    
+    //cl = clock();
+    for(int i = 0; i < num_threads; i++){
 
-    //if( !(isprime(p) or isprime(q))){
-    //  printf("SOMETHING WENT WRONG\n");
-    // }
-    gmp_printf("n: %Zd\n", n);
-    gmp_printf("p: %Zd\n", p);
-    gmp_printf("q: %Zd\n", q);
-    */
+       threads[i] = std::thread([i, &n, &p, &running, &cond]{
+	   std::cout << "Thread " << i << " starting" << std::endl;
+	   mpz_class x = rand()%n + 1;
+	   mpz_class y = x;
+	   mpz_class c = rand()&n + 1; 
+	   mpz_class g = 1;
+
+	   //std::cout << "x: " << x << " y: " << y << " c: " << c << std::endl;
+
+	   while(g==1){
+	     if (!running.load()){
+	       return;
+	     }
+	     
+	     x = ( (x*x)%n + c)%n;    
+	     y = ( (y*y)%n + c)%n;
+	     y = ( (y*y)%n + c)%n;
+	     g = gcd(abso(x-y),  n);
+	     
+	   }
+	   std::cout << "THE LOOP FINISHED thread " << i << " found " << g << std::endl;
+	   running = false;
+	   cond.notify_all();
+	   p = g;
+	   return;
+	 });
+       threads[i].detach();
+    }
+
+    std::unique_lock<std::mutex> lock{ m };
+    // Wait until any of the threads signals us. p MUST be captured as a reference, since the value  in the lambda                                                                                        
+    // is not updated every time we evaluate the wait                                                
+    cond.wait(lock, [&p] {
+        return p != 0;
+    });
+
+    std::cout << "THE END I GUESS"<< std::endl;
+    std::cout << "We Found " << p << " and " << n/p <<  std::endl;
+    
       
-	return 0;
+    return 0;
 }
