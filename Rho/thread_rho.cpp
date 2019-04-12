b9461968a0d79699cdb95d3e1f88c5eddf2ba3c8
Ioannis Boutsikas <mgioanni@outlook.com>
2019-04-12 15:22:28 -0400

    Add internal timing to Rho

--- Rho/thread_rho.cpp
+++ Rho/thread_rho.cpp
@@ -8,111 +8,113 @@
 #include <mutex>
 #include <condition_variable>
 #include <iostream>
-/*
-bool isprime(mpz_class n){
-  if (n == 2){return true; }
-  if (n == 3){return true; }
-  if (n % 2 == 0){return false;}
-  if (n % 3 == 0){return false;}
+#include <chrono>
 
-  mpz_class i = 5;
-  mpz_class w = 2;
-
-  while (i * i <= n){
-    if (n % i == 0){printf("IS NOT THE FATHER\n");return false;}
-    i += w;
-    w = 6 - w;
-  }
-  printf("IS THE FATHER\n");
-  return true;
-
-}
-*/
-
-mpz_class abso(mpz_class a)     //fn to return absolute value
+mpz_class abso(mpz_class a) //fn to return absolute value
 {
-    return a>0?a:-a;
+  return a > 0 ? a : -a;
 }
 int recur_depth = 0;
 
-mpz_class gcd(mpz_class a,mpz_class b)    //Euclidean GCD recursive fn
+mpz_class gcd(mpz_class a, mpz_class b) //Euclidean GCD recursive fn
 {
-    if(b==0)
-        return a;
-    else{
-      return gcd(b,a%b);
-    }
+  if (b == 0)
+    return a;
+  else
+  {
+    return gcd(b, a % b);
+  }
 }
 
-
-
-int main(int argc, char *argv[])  //Driver Program
+int main(int argc, char *argv[]) //Driver Program
 {
-    srand(time(NULL));
-    if(argc != 3){
-      std::cout << "Two arguments plz" << std::endl;
-      std::cout << "1: the number to factor, 2: the number of threads" << std::endl;
-      return -1;
-    }
-    mpz_class n(argv[1]);
-    int num_threads = atoi(argv[2]);
-    mpz_class p = 0;
-    mpz_class q;
+  srand(time(NULL));
+  if (argc != 3)
+  {
+    std::cout << "Two arguments plz" << std::endl;
+    std::cout << "1: the number to factor, 2: the number of threads" << std::endl;
+    return -1;
+  }
+  mpz_class n(argv[1]);
+  int num_threads = atoi(argv[2]);
+  mpz_class p = 0;
+  mpz_class q;
 
-    std::mutex m;
-    std::condition_variable cond;
-    std::atomic<bool> running(true);
+  std::mutex m;
+  std::condition_variable cond;
+  std::atomic<bool> running(true);
 
-    if(n%2==0){
-      printf("EVEN\n");
-      return -1;
-    }
-    
-    std::thread threads[num_threads];
-    std::cout << "Starting threads for number " << n << std::endl;
-    
-    //cl = clock();
-    for(int i = 0; i < num_threads; i++){
+  if (n % 2 == 0)
+  {
+    printf("EVEN\n");
+    return -1;
+  }
 
-       threads[i] = std::thread([i, &n, &p, &running, &cond]{
-	   std::cout << "Thread " << i << " starting" << std::endl;
-	   mpz_class x = rand()%n + 1;
-	   mpz_class y = x;
-	   mpz_class c = rand()&n + 1; 
-	   mpz_class g = 1;
+  auto start = std::chrono::steady_clock::now();
 
-	   //std::cout << "x: " << x << " y: " << y << " c: " << c << std::endl;
+  std::thread threads[num_threads];
+  std::cout << "Starting threads for number " << n << std::endl;
 
-	   while(g==1){
-	     if (!running.load()){
-	       return;
-	     }
-	     
-	     x = ( (x*x)%n + c)%n;    
-	     y = ( (y*y)%n + c)%n;
-	     y = ( (y*y)%n + c)%n;
-	     g = gcd(abso(x-y),  n);
-	     
-	   }
-	   std::cout << "THE LOOP FINISHED thread " << i << " found " << g << std::endl;
-	   running = false;
-	   cond.notify_all();
-	   p = g;
-	   return;
-	 });
-       threads[i].detach();
-    }
+  //cl = clock();
+  for (int i = 0; i < num_threads; i++)
+  {
 
-    std::unique_lock<std::mutex> lock{ m };
-    // Wait until any of the threads signals us. p MUST be captured as a reference, since the value  in the lambda                                                                                        
-    // is not updated every time we evaluate the wait                                                
-    cond.wait(lock, [&p] {
-        return p != 0;
+    threads[i] = std::thread([i, &n, &p, &running, &cond] {
+      std::cout << "Thread " << i << " starting" << std::endl;
+      mpz_class x = rand() % n + 1;
+      mpz_class y = x;
+      mpz_class c = rand() & n + 1;
+      mpz_class g = 1;
+
+      //std::cout << "x: " << x << " y: " << y << " c: " << c << std::endl;
+
+      while (g == 1)
+      {
+        if (!running.load())
+        {
+          return;
+        }
+
+        x = ((x * x) % n + c) % n;
+        y = ((y * y) % n + c) % n;
+        y = ((y * y) % n + c) % n;
+        g = gcd(abso(x - y), n);
+      }
+      p = g;
+      std::cout << "THE LOOP FINISHED thread " << i << " found " << g << std::endl;
+      running = false;
+      cond.notify_all();
+      return;
     });
 
-    std::cout << "THE END I GUESS"<< std::endl;
-    std::cout << "We Found " << p << " and " << n/p <<  std::endl;
-    
-      
-    return 0;
+    threads[i].detach();
+  }
+
+  std::unique_lock<std::mutex> lock{m};
+  // Wait until any of the threads signals us. p MUST be captured as a reference, since the value  in the lambda
+  // is not updated every time we evaluate the wait
+  cond.wait(lock, [&p] {
+    return p != 0;
+  });
+
+  mpz_class q = n / p;
+
+  auto end = std::chrono::steady_clock::now();
+
+  std::cout << "n: " << n << std::endl;
+  std::cout << "p: " << p << std::endl;
+  std::cout << "q: " << q << std::endl;
+  std::cout << "Elapsed time in milliseconds : "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+            << " ms" << std::endl;
+
+  std::cout << "Elapsed time in seconds : "
+            << std::chrono::duration_cast<std::chrono::seconds>(end - start).count()
+            << " sec" << std::endl;
+
+  std::cout << "Elapsed time in minutes : "
+            << std::chrono::duration_cast<std::chrono::minutes>(end - start).count()
+            << " mins" << std::endl;
+
+  return 0;
 }
