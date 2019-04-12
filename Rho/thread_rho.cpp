126b4cf3aaaaf214eb012289896c4aec3e2134d7
Ioannis Boutsikas <mgioanni@outlook.com>
2019-04-12 18:29:53 -0400

    Add guarded output to rho

--- Rho/thread_rho.cpp
+++ Rho/thread_rho.cpp
@@ -1,7 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include <gmpxx.h>
-
 #include <thread>
 #include <atomic>
 #include <ctime>
@@ -10,6 +9,7 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+
 mpz_class abso(mpz_class a) //fn to return absolute value
 {
   return a > 0 ? a : -a;
@@ -28,99 +28,115 @@ mpz_class gcd(mpz_class a, mpz_class b) //Euclidean GCD recursive fn
 
 int main(int argc, char *argv[]) //Driver Program
 {
-    srand(time(NULL));
-    if (argc < 2 || argc > 3) {
-      std::cout << "WRONG" << std::endl;
-        exit(0);
+  srand(time(NULL));
+  if (argc < 2 || argc > 3)
+  { 
+    #if defined(LOG_OUTPUT)
+    std::cout << "WRONG" << std::endl;
+    #endif
+    exit(0);
+  }
+
+  int num_threads = std::thread::hardware_concurrency();
+
+  if (argc == 3)
+  {
+    std::string arg = argv[2];
+    try
+    {
+      std::size_t pos;
+      int x = std::stoi(arg, &pos);
+      num_threads = x;
     }
+    catch (std::invalid_argument const &ex)
+    {
+      std::cerr << "Invalid threads option: " << arg << ". Assuming " << num_threads << " thrads" << std::endl;
+    }
+    catch (std::out_of_range const &ex)
+    {
+      std::cerr << "Invalid threads option: " << arg << ". Assuming " << num_threads << " threads" << std::endl;
+    }
+  }
 
-    int num_threads = std::thread::hardware_concurrency();
+  mpz_class n(argv[1], 10);
 
-    if (argc == 3) {
-        std::string arg = argv[2];
-        try {
-            std::size_t pos;
-            int x = std::stoi(arg, &pos);
-            num_threads = x;
+  mpz_class p = 0;
+  mpz_class q;
+
+  std::mutex m;
+  std::condition_variable cond;
+  std::atomic<bool> running(true);
+
+  if (n % 2 == 0)
+  {
+    #if defined(LOG_OUTPUT)
+    printf("EVEN\n");
+    #endif
+    return -1;
+  }
+
+  std::vector<std::thread> threads;
+
+  threads.reserve(num_threads);
+  #if defined(LOG_OUTPUT)
+  std::cout << "Starting " << num_threads << "  threads for number " << n << std::endl;
+  #endif
+  
+  auto start = std::chrono::steady_clock::now();
+  for (int i = 0; i < num_threads; i++)
+  {
+
+    threads[i] = std::thread([i, &n, &p, &running, &cond] {
+      //std::cout << "Thread " << i << " starting" << std::endl;
+      mpz_class x; // = rand()%n + 1;
+      mpz_class y; // = x;
+      mpz_class c; // = rand()&n + 1;
+      mpz_class g = n;
+
+      //std::cout << "x: " << x << " y: " << y << " c: " << c << std::endl;
+
+      while (g == n)
+      {
+        x = rand() % n + 1;
+        y = x;
+        c = rand() & n + 1;
+        g = 1;
+        #if defined(LOG_OUTPUT)
+        std::cout << "Thread " << i << " LOOPED" << std::endl;
+        #endif
+        while (g == 1)
+        {
+          if (!running.load())
+          {
+            return;
+          }
+
+          x = ((x * x) % n + c) % n;
+          y = ((y * y) % n + c) % n;
+          y = ((y * y) % n + c) % n;
+          g = gcd(abs(x - y), n);
         }
-        catch (std::invalid_argument const &ex) {
-            std::cerr << "Invalid threads option: " << arg << ". Assuming " << num_threads << " thrads" << std::endl;
-        }
-        catch (std::out_of_range const &ex) {
-            std::cerr << "Invalid threads option: " << arg << ". Assuming " << num_threads << " threads" << std::endl;
-        }
-    }
+      }
+      p = g;
+      #if defined(LOG_OUTPUT)
+      std::cout << "THE LOOP FINISHED thread " << i << " found " << g << std::endl;
+      #endif
+      running = false;
+      cond.notify_all();
 
-
-    mpz_class n(argv[1], 10);
-
-    mpz_class p = 0;
-    mpz_class q;
-
-    std::mutex m;
-    std::condition_variable cond;
-    std::atomic<bool> running(true);
-
-    if(n%2==0){
-      printf("EVEN\n");
-      return -1;
-    }
-    
-    std::vector<std::thread> threads;
-
-    threads.reserve(num_threads);
-    
-    std::cout << "Starting " << num_threads  << "  threads for number " << n << std::endl;
-    
-    //cl = clock();
-    auto start = std::chrono::steady_clock::now();
-    for(int i = 0; i < num_threads; i++){
-
-       threads[i] = std::thread([i, &n, &p, &running, &cond]{
-	   //std::cout << "Thread " << i << " starting" << std::endl;
-	   mpz_class x;// = rand()%n + 1;
-	   mpz_class y;// = x;
-	   mpz_class c;// = rand()&n + 1; 
-	   mpz_class g = n;
-
-	   //std::cout << "x: " << x << " y: " << y << " c: " << c << std::endl;
-
-	   while( g == n){
-	     x = rand()%n + 1;
-	     y = x;
-	     c = rand()&n + 1; 
-	     g = 1;
-	     std::cout << "Thread " << i << " LOOPED" << std::endl;
-	     while(g==1){
-	       if (!running.load()){
-		 return;
-	       }
-	       
-	       x = ( (x*x)%n + c)%n;    
-	       y = ( (y*y)%n + c)%n;
-	       y = ( (y*y)%n + c)%n;
-	       g = gcd(abs(x-y),  n);
-	     }
-	   }
-	   p = g;
-	   std::cout << "THE LOOP FINISHED thread " << i << " found " << g << std::endl;
-	   running = false;
-	   cond.notify_all();
-
-	   return;
-	 });
-       
-       threads[i].detach();
-    }
-
-    std::unique_lock<std::mutex> lock{ m };
-    // Wait until any of the threads signals us. p MUST be captured as a reference, since the value  in the lambda                                                                                        
-    // is not updated every time we evaluate the wait                                                
-    cond.wait(lock, [&p] {
-        return p != 0;
+      return;
     });
 
-    
+    threads[i].detach();
+  }
+
+  std::unique_lock<std::mutex> lock{m};
+  // Wait until any of the threads signals us. p MUST be captured as a reference, since the value  in the lambda
+  // is not updated every time we evaluate the wait
+  cond.wait(lock, [&p] {
+    return p != 0;
+  });
+
   q = n / p;
 
   auto end = std::chrono::steady_clock::now();
