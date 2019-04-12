20436afe9febc57acf0e8936c2eb60a7fcfe232a
thomasf2 <thomasf2@umbc.edu>
2019-04-12 15:34:52 -0400

    added vector and fixed bugs (probably)

--- Rho/thread_rho.cpp
+++ Rho/thread_rho.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <iostream>
+#include <vector>
 /*
 bool isprime(mpz_class n){
   if (n == 2){return true; }
@@ -29,12 +30,14 @@ bool isprime(mpz_class n){
 }
 */
 
+
+/*
 mpz_class abso(mpz_class a)     //fn to return absolute value
 {
     return a>0?a:-a;
 }
 int recur_depth = 0;
-
+*/
 mpz_class gcd(mpz_class a,mpz_class b)    //Euclidean GCD recursive fn
 {
     if(b==0)
@@ -49,13 +52,33 @@ mpz_class gcd(mpz_class a,mpz_class b)    //Euclidean GCD recursive fn
 int main(int argc, char *argv[])  //Driver Program
 {
     srand(time(NULL));
-    if(argc != 3){
-      std::cout << "Two arguments plz" << std::endl;
-      std::cout << "1: the number to factor, 2: the number of threads" << std::endl;
-      return -1;
+    if (argc < 2 || argc > 3) {
+      std::cout << "WRONG" << std::endl;
+        exit(0);
     }
-    mpz_class n(argv[1]);
-    int num_threads = atoi(argv[2]);
+
+    int num_threads = std::thread::hardware_concurrency();
+
+    if (argc == 3) {
+        std::string arg = argv[2];
+        try {
+            std::size_t pos;
+            int x = std::stoi(arg, &pos);
+            num_threads = x;
+        }
+        catch (std::invalid_argument const &ex) {
+            std::cerr << "Invalid threads option: " << arg << ". Assuming " << num_threads << " thre\
+ads" << std::endl;
+        }
+        catch (std::out_of_range const &ex) {
+            std::cerr << "Invalid threads option: " << arg << ". Assuming " << num_threads << " thre\
+ads" << std::endl;
+        }
+    }
+
+
+    mpz_class n(argv[1], 10);
+
     mpz_class p = 0;
     mpz_class q;
 
@@ -68,31 +91,40 @@ int main(int argc, char *argv[])  //Driver Program
       return -1;
     }
     
-    std::thread threads[num_threads];
-    std::cout << "Starting threads for number " << n << std::endl;
+    std::vector<std::thread> threads;
+
+    threads.reserve(num_threads);
+    
+    std::cout << "Starting " << num_threads  << "  threads for number " << n << std::endl;
     
     //cl = clock();
     for(int i = 0; i < num_threads; i++){
 
        threads[i] = std::thread([i, &n, &p, &running, &cond]{
-	   std::cout << "Thread " << i << " starting" << std::endl;
-	   mpz_class x = rand()%n + 1;
-	   mpz_class y = x;
-	   mpz_class c = rand()&n + 1; 
-	   mpz_class g = 1;
+	   //std::cout << "Thread " << i << " starting" << std::endl;
+	   mpz_class x;// = rand()%n + 1;
+	   mpz_class y;// = x;
+	   mpz_class c;// = rand()&n + 1; 
+	   mpz_class g = n;
 
 	   //std::cout << "x: " << x << " y: " << y << " c: " << c << std::endl;
 
-	   while(g==1){
-	     if (!running.load()){
-	       return;
+	   while( g == n){
+	     x = rand()%n + 1;
+	     y = x;
+	     c = rand()&n + 1; 
+	     g = 1;
+	     std::cout << "Thread " << i << " LOOPED" << std::endl;
+	     while(g==1){
+	       if (!running.load()){
+		 return;
+	       }
+	       
+	       x = ( (x*x)%n + c)%n;    
+	       y = ( (y*y)%n + c)%n;
+	       y = ( (y*y)%n + c)%n;
+	       g = gcd(abs(x-y),  n);
 	     }
-	     
-	     x = ( (x*x)%n + c)%n;    
-	     y = ( (y*y)%n + c)%n;
-	     y = ( (y*y)%n + c)%n;
-	     g = gcd(abso(x-y),  n);
-	     
 	   }
 	   std::cout << "THE LOOP FINISHED thread " << i << " found " << g << std::endl;
 	   running = false;
